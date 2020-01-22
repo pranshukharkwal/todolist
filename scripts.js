b95c8485887b70bdcde0a5a05f2b0a8c9cf189e8
Pranshu Kharkwal <awesomepranshu@gmail.com>
2020-01-22 16:47:48 +0530

    Fixed the empty task problem

--- scripts.js
+++ scripts.js
@@ -3,21 +3,30 @@ var item = document.getElementsByTagName("li")
 var input = document.getElementById("task")
 var button = document.getElementById("enter")
 
-function createItem() {
+button.onclick = function(){
 
-    var li = document.createElement("li");
-    var abc = document.createTextNode(input.value);
-    li.appendChild(abc);
-    var dbtn = document.createElement("button")
-    dbtn.appendChild(document.createTextNode('X'))
-    li.appendChild(dbtn)
-    ul.appendChild(li)  
-    dbtn.onclick = function(){
-        li.style.display = "none";
-        li.classList.add("delete")
+    if(input.value.length <= 0)
+    {
+        alert("Cannot add an empty task")
     }
+
+    else {
+
+        var li = document.createElement("li");
+        var abc = document.createTextNode(input.value);
+        li.appendChild(abc);
+        var dbtn = document.createElement("button")
+        dbtn.appendChild(document.createTextNode('X'))
+        li.appendChild(dbtn)
+        ul.appendChild(li)  
+        
+        dbtn.onclick = function(){
+            li.style.display = "none";
+            li.classList.add("delete")
+        }
+    }
+    
     input.value = ""
     
-        
 }
 
